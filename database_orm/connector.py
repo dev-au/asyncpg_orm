8e6ae1deb71ea3759cea1da1828f255ada8166fb
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-05-08 02:49:11 +0500

    Initial commit

---
+++ database_orm/connector.py
@@ -0,0 +1,1 @@
+class PostgreSQL:    def __init__(self, database_user: str,                 database_password: str,                 database_host: str,                 database_name: str):        self.database_user = database_user        self.database_password = database_password        self.database_host = database_host        self.database_name = database_name
